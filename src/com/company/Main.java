ce97100e38af5d13eb9bb32682673e272a6764a6
DrPhannibal <mahdisouri0@gmail.com>
2022-05-03 23:02:28 +0430

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int myInt;
+        long myLong = 123456789123456789L;
+
+//        myInt = (int) myLong;
+//        System.out.println(myInt);
+
+        char myCh='a';
+
+        myInt=myCh;
+
+        System.out.println(myInt);
+    }
+}
